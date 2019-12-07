1e384b7340bbb2fc11301ac32e1896b85a754fd1
Lucas Anderson <landerson_khs@hotmail.com>
2019-12-07 15:11:30 -0800

    Fix import file pathing

--- classes/sentinal.py
+++ classes/sentinal.py
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 from datetime import datetime
-=======
-import os
->>>>>>> User May Capture Images From RaspberryPi Camera
 
 class Sentinal:
     def __init__(self, camera):
         self.camera = camera
 
-<<<<<<< HEAD
     def capture(self):
         self.camera.start_preview()
         self.camera.capture('./images/image_%s.jpg' % self.__seconds())
@@ -16,12 +11,3 @@ class Sentinal:
 
     def __seconds(self):
         return (datetime.now() - datetime(2019, 1, 1)).seconds
-=======
-    def capture():
-        self.camera.start_preview()
-        self.camera.capture('./../images/image_%s.jpg', self.seconds())
-        self.camera.stop_preview()
-
-    def __seconds():
-        (datetime.datetime.now() - datetime.datetime(2019, 1, 1)).seconds
->>>>>>> User May Capture Images From RaspberryPi Camera
