ebe89696dfca21179ecfd243f879672785a19bf5
Lucas Anderson <landerson_khs@hotmail.com>
2019-12-09 17:58:13 -0800

    Fix loop error Format main Add description to compare

--- classes/sentinal.py
+++ classes/sentinal.py
@@ -1,20 +1,26 @@
 from skimage.measure import compare_ssim
 import cv2
 from datetime import datetime
+from time import sleep
 
 class Sentinal:
     def __init__(self, camera):
         self.camera = camera
+        self.camera.rotation = 180
 
     def capture(self):
         self.camera.start_preview()
+        sleep(2)
         self.camera.capture('./images/image_%s.jpg' % self.__seconds())
         self.camera.stop_preview()
 
+# Score range [-1, 1] with a value of one being a “perfect match”.
+# Diff contains the actual image differences between the two input images that
+# we wish to visualize.
     def compare(self, images):
         img_one, img_two, img_three = self.__convert_images(images)
         (score, diff) = compare_ssim(img_one, img_two, full=True)
-        print(score)
+        print(f'Score: {score}\tDifference: {diff}')
 
     def __convert_images(self, images):
         return self.__grayscale([cv2.imread(image) for image in images])
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from glob import glob
+import os
 from picamera import PiCamera
 from classes.sentinal import Sentinal
-from time import sleep
 
 def main():
-    camera = PiCamera()
-    camera.rotation = 180
+    image_path = './images/*.jpg'
+    sentinal = Sentinal(PiCamera())
 
-    sentinal = Sentinal(camera)
+    for image in glob(image_path):
+        os.remove(image)
 
-    images = [image for image in glob('./images/*.jpg')]
-
-    while len(images) < 3:
+    for x in range(3):
         sentinal.capture()
+
+    images = [image for image in glob(image_path)]
 
     sentinal.compare(images)
 
