a8eb0e0ebf0917ca35391affc4760a52addd4c48
Lucas Anderson <landerson_khs@hotmail.com>
2019-12-10 21:54:20 -0800

    Compare Images and Record Movement (#5)
    Add method to compare the differences between images
    so that the program may determine if the was any
    movement detected.
    
    Closes #2
    Closes #3

--- classes/sentinal.py
+++ classes/sentinal.py
@@ -1,13 +1,45 @@
+from skimage.metrics import structural_similarity
+import cv2
 from datetime import datetime
+from time import sleep
 
 class Sentinal:
     def __init__(self, camera):
         self.camera = camera
+        self.camera.rotation = 180
 
     def capture(self):
-        self.camera.start_preview()
-        self.camera.capture('./images/image_%s.jpg' % self.__seconds())
+        filename = './images/image_%s.jpg' % self.__seconds()
+        self.camera.capture(filename)
+        return filename
+
+# Score range [-1, 1] with a value of one being a “perfect match”.
+# Diff contains the actual image differences between the two input images that
+# we wish to visualize.
+    def compare(self, images):
+        img_one, img_two, img_three = self.__convert_images(images)
+
+        (score1, diff1) = structural_similarity(img_one, img_two, full=True)
+        (score2, diff2) = structural_similarity(img_two, img_three, full=True)
+
+        print(f'Score1: {score1}\tScore2: {score2}',
+              f'\tDifference: {abs(score1 - score2)}')
+
+        if(abs(score1 - score2) > 0.002):
+            print("Movement")
+
+    def off_duty(self):
         self.camera.stop_preview()
+
+    def on_duty(self):
+        self.camera.start_preview()
+        sleep(3)
+
+    def __convert_images(self, images):
+        return self.__grayscale([cv2.imread(image) for image in images])
+
+    def __grayscale(self, images):
+        return [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in images]
 
     def __seconds(self):
         return (datetime.now() - datetime(2019, 1, 1)).seconds
--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+from datetime import datetime
+from glob import glob
+import os
 from picamera import PiCamera
 from classes.sentinal import Sentinal
 from time import sleep
 
 def main():
-    camera = PiCamera()
-    camera.rotation = 180
+    print('Start program:', datetime.now())
+    image_path = './images/*.jpg'
 
-    sentinal = Sentinal(camera)
-    sentinal.capture()
+    for image in glob(image_path):
+        os.remove(image)
+
+    print('Start sentinal:', datetime.now())
+    sentinal = Sentinal(PiCamera())
+    sentinal.on_duty()
+
+    print('Start capture:', datetime.now())
+    for x in range(3):
+        sentinal.capture()
+        sleep(2)
+
+    images = [image for image in glob(image_path)]
+    images.sort(reverse=False)
+
+    print(images)
+
+    print('Start compare:', datetime.now())
+    for x in range(20):
+        print('Comparing:', datetime.now())
+        sentinal.compare(images)
+        os.remove(images.pop(0))
+        images.append(sentinal.capture())
+
+    print('Start Off Duty:', datetime.now())
+    sentinal.off_duty()
+    print('End program:', datetime.now())
 
 if __name__== "__main__":
     main()
