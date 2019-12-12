fa2290af23cf0be5033550ae4f2dd06dada1b6f2
Lucas Anderson <landerson_khs@hotmail.com>
2019-12-12 01:19:36 -0800

    Add comments for program Add infinite loop until enter key pressed

--- classes/sentinal.py
+++ classes/sentinal.py
@@ -4,9 +4,14 @@ from datetime import datetime
 from time import sleep
 import yaml
 
+# Compares images to determine of there is any movement detected. Once detected
+# it triggers the camera to start recording and sends a message to owner of
+# movement being detected. Once there is no more movement detected, it sends
+# another email about uploading a video to AWS S3 bucket.
 class Sentinal:
     send_message = True
 
+    # Inialize camera, watchmen and dispatcher. Rotates camera 180 degrees.
     def __init__(self, camera, dispatcher):
         self.camera = camera
         self.camera.rotation = 180
@@ -20,18 +25,26 @@ class Sentinal:
         self.camera.capture(filename)
         return filename
 
-# Score range [-1, 1] with a value of one being a “perfect match”.
-# Diff contains the actual image differences between the two input images that
-# we wish to visualize.
+    # Used to compare a list of images to detect movement. If movement is found
+    # then an email is sent and the camera is told to start recording
     def compare(self, images):
+        # Convert images to grayscale
         img_one, img_two, img_three = self.__convert_images(images)
 
+        # Score range [-1, 1] with a value of one being a “perfect match”.
+        # Diff contains the actual image differences between the two input
+        # images that we wish to visualize.
         (score1, diff1) = structural_similarity(img_one, img_two, full=True)
         (score2, diff2) = structural_similarity(img_two, img_three, full=True)
 
+        # Prints the pair image score (img_one, img_two), (img_two, img_three)
+        # and then displays the abs score difference between the two
         print(f'Score1: {score1}\tScore2: {score2}',
               f'\tDifference: {abs(score1 - score2)}')
 
+        # Checks to see if score difference is over the threshold, which
+        # indicates there is movement. Once True, then a message is sent and
+        # the video will start recording and an email is sent.
         if(abs(score1 - score2) > 0.002):
             if(self.send_message):
                 print("Message Dispatched")
@@ -40,28 +53,36 @@ class Sentinal:
             else:
                 print("Movement")
 
+    # Turns off camera
     def off_duty(self):
         self.camera.stop_preview()
 
+    # Turns on camera
     def on_duty(self):
         self.camera.start_preview()
         sleep(3)
 
+    # Returns grayscaled images
     def __convert_images(self, images):
         return self.__grayscale([cv2.imread(image) for image in images])
 
+    # Detection message used for email
     def __detection_message(self):
         subject = self.application['MOVEMENT_SUBJECT']
         message = self.application['MOVEMENT_MESSAGE']
         return f'Subject: {subject}\r\n{message}'
 
+    # Grayscales images
     def __grayscale(self, images):
         return [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in images]
 
+    # Recorded message used for email
     def __recording_message(self):
         subject = self.application['RECORDING_SUBJECT']
         message = self.application['RECORDING_MESSAGE']
         return f'Subject: {subject}\r\n{message}'
 
+    # Gets the current seconds from the beginning of the year to uniquely name
+    # images.
     def __seconds(self):
         return (datetime.now() - datetime(2019, 1, 1)).seconds
--- main.py
+++ main.py
@@ -3,39 +3,60 @@ from classes.dispatcher import Dispatcher
 from glob import glob
 import os
 from picamera import PiCamera
+from select import select
 from classes.sentinal import Sentinal
+import sys
 from time import sleep
 
+# Home security program that compares 3 images to see if there is enough of a
+# difference to be classified as movement. If there is movement detected, the
+# camera starts recording and an email sends an email to the building's owner.
+# Once movement stops, the camera stops recording and the video gets uploaded
+# to an AWS S3 bucket. Another email is sent to owner about the video being
+# stored online so they can view it.
 def main():
     print('Start program:', datetime.now())
-    image_path = './images/*.jpg'
+    image_path = './images/*.jpg' # Path to images folder
 
+    # Delete any images in the images folder
     for image in glob(image_path):
         os.remove(image)
 
     print('Start sentinal:', datetime.now())
+    # Initialize Sentinal with camera, dispatcher and watchmen instance
     sentinal = Sentinal(PiCamera(), Dispatcher())
-    sentinal.on_duty()
+    sentinal.on_duty() # Turn camera on
 
+    # Take first three photos every 2 seconds
     print('Start capture:', datetime.now())
     for x in range(3):
         sentinal.capture()
         sleep(2)
 
+    # Load images into List and make sure they're not reordered
     images = [image for image in glob(image_path)]
     images.sort(reverse=False)
 
     print(images)
 
+    # Loop continuously to detect movement by taking pictures after each
+    # comparison. Once the images are compared, the older image is deleted and
+    # a new picture is taken and added to the List.
     print('Start compare:', datetime.now())
-    for x in range(20):
+    while True:
+        # Used to stop loop when enter key is pressed
+        time.sleep(1)
+        i,o,e = select([sys.stdin],[],[],0.0001)
+        if i == [sys.stdin]: break
+
+        # Compare and then capture new images
         print('Comparing:', datetime.now())
         sentinal.compare(images)
         os.remove(images.pop(0))
         images.append(sentinal.capture())
 
     print('Start Off Duty:', datetime.now())
-    sentinal.off_duty()
+    sentinal.off_duty() # Turn off camera
     print('End program:', datetime.now())
 
 if __name__== "__main__":
